76d5ba199b831db84b9613958e5895201eda48ed
joss-c <joss.classey@gmail.com>
2019-11-14 13:49:01 +0000

    Updates

---
+++ precache-manifest.bb46ce9f6f43bd1c5be6b9d80edceb25.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9b46fb9ba85f80dc4349e7296c5c13a",
+    "url": "/surprise-toy-reality/index.html"
+  },
+  {
+    "revision": "1a44cb66e67183fcf43d",
+    "url": "/surprise-toy-reality/static/css/main.2680dfa7.chunk.css"
+  },
+  {
+    "revision": "b2886b475d74905f3382",
+    "url": "/surprise-toy-reality/static/js/2.2ee3a94b.chunk.js"
+  },
+  {
+    "revision": "1a44cb66e67183fcf43d",
+    "url": "/surprise-toy-reality/static/js/main.103ddd4b.chunk.js"
+  },
+  {
+    "revision": "3cafa003c922000b0700",
+    "url": "/surprise-toy-reality/static/js/runtime-main.a4eec85f.js"
+  },
+  {
+    "revision": "803b47eea9055abb0e701d747436d860",
+    "url": "/surprise-toy-reality/static/media/buckley.803b47ee.jpg"
+  },
+  {
+    "revision": "1af47343deef8c59b269ba6aaa34aeb4",
+    "url": "/surprise-toy-reality/static/media/critters.1af47343.jpg"
+  },
+  {
+    "revision": "8912e0a63bcebab9dd1ec39ab3bf16ca",
+    "url": "/surprise-toy-reality/static/media/hopscotch.8912e0a6.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/surprise-toy-reality/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "c529d8bf446b0f78c21391c78f15d42e",
+    "url": "/surprise-toy-reality/static/media/mystery.c529d8bf.jpg"
+  },
+  {
+    "revision": "eb2a0a81c508d4147aaf535f92e08359",
+    "url": "/surprise-toy-reality/static/media/outback.eb2a0a81.jpg"
+  },
+  {
+    "revision": "ebccb1530f39c0bea845c45b94f9a187",
+    "url": "/surprise-toy-reality/static/media/packet.ebccb153.jpg"
+  },
+  {
+    "revision": "0bf85a37931445d843b5864960f00ac1",
+    "url": "/surprise-toy-reality/static/media/polar.0bf85a37.jpg"
+  },
+  {
+    "revision": "699d76fe59116eb143fd6abd988f95fd",
+    "url": "/surprise-toy-reality/static/media/silk.699d76fe.jpg"
+  },
+  {
+    "revision": "0d6ebe97ca8d0699d3af380df793963b",
+    "url": "/surprise-toy-reality/static/media/sweetpea.0d6ebe97.jpg"
+  },
+  {
+    "revision": "a5e8501bd0cc4ac340ba79d461e81746",
+    "url": "/surprise-toy-reality/static/media/tuxedo.a5e8501b.jpg"
+  },
+  {
+    "revision": "e80e7202d60a9b61c6136b1e6fccbc6a",
+    "url": "/surprise-toy-reality/static/media/wilder.e80e7202.jpg"
+  }
+]);

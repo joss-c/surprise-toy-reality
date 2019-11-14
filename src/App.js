d3493be96793ea3b5680d47377549c1e8d6c23f5
joss-c <joss.classey@gmail.com>
2019-11-14 13:18:13 +0000

    feat(App): Complete MVP

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
+import packet from './img/packet.jpg'
 import './App.css';
 
+const names = {
+  1: 'buckley',
+  2: 'hopscotch',
+  3: 'outback',
+  4: 'polar',
+  5: 'silk',
+  6: 'sweetpea',
+  7: 'tuxedo',
+  8: 'wilder',
+  9: 'mystery'
+}
+
+const selectCritter = () => {
+  const secretToy = Math.floor(Math.random() * 27) + 1
+  console.log(secretToy)
+  if (secretToy === 27) return 9
+  else return Math.floor(Math.random() * 8) + 1
+}
+
 function App() {
+  const [critters, createCritter] = useState([])
+  console.log(critters)
+  const handleClick = () => createCritter(prevState => [...prevState, selectCritter()])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ padding: '1rem' }}>
+        {critters.map(critter => {
+          return <img style={{ width: '2rem' }} src={require(`./img/${names[critter]}.jpg`)}></img>
+        })}
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          padding: '2rem'
+      }}>
+        <img style={{ cursor: 'pointer' }} src={packet} onClick={() => handleClick()}></img>
+      </div>
     </div>
   );
 }

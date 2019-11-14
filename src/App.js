159ef1095b6a7f7d42c16f6d57c536e60ae54e43
joss-c <joss.classey@gmail.com>
2019-11-14 19:59:04 +0000

    feat(App): Add money wasted stat

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import packet from './img/packet.jpg'
 import './App.css';
@@ -15,17 +15,29 @@ const names = {
   9: 'mystery'
 }
 
-const selectCritter = () => {
-  const secretToy = Math.floor(Math.random() * 27) + 1
-  console.log(secretToy)
-  if (secretToy === 27) return 9
-  else return Math.floor(Math.random() * 8) + 1
-}
-
 function App() {
   const [critters, createCritter] = useState([])
   const [moneySpent, addMoney] = useState(0)
-  console.log(critters)
+  const [countCritters, addCritter] = useState(Array.from(Array(9), () => 0))
+  const [moneyWasted, setMoneyWasted] = useState(0)
+
+  const selectCritter = () => {
+    const secretToy = Math.floor(Math.random() * 27) + 1
+    if (secretToy === 27) {
+      addCritter(prevState => {
+        prevState[8]++
+        return prevState
+      })
+      return 9
+    }
+    const critter = Math.floor(Math.random() * 8) + 1
+    addCritter(prevState => {
+      prevState[critter - 1]++
+      return prevState
+    })
+    return critter
+  }
+
   const handleClick = () => {
     addMoney(prevState => prevState += 7)
     createCritter(prevState => [...prevState, selectCritter()])
@@ -33,13 +45,27 @@ function App() {
   const hasFullSet = () => {
     const total = new Set(critters)
     const result = [...total].reduce((a, b) => a + b, 0)
-    return (result === 45) ? 'YES' : 'NO'
+    return (result === 45) ? ['YES', 'green'] : ['NO', 'red']
   }
+  useEffect(() => {
+    setMoneyWasted(countCritters.reduce((sum, critter) => {
+      if (critter > 1) sum += (critter - 1) * 7
+      return sum
+    }, 0))
+    console.log('CHECK IT', countCritters)
+  }, [moneySpent])
   return (
     <div style={{ padding: '1rem' }}>
       <div>
-        <p>{`Money spent: $${moneySpent}`}</p>
-        <p>{`Full set? ${hasFullSet()}`}</p>
+        <div style={{ display: 'flex' }}>
+          <div>
+            {`Money spent: $${moneySpent}`}
+          </div>
+          <div style={{ paddingLeft: '1rem' }}>
+            {`Money wasted: $${moneyWasted}`}
+          </div>
+        </div>
+        <p>Full set? <span style={{ color: hasFullSet()[1] }}>{hasFullSet()[0]}</span></p>
       </div>
       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
         {critters.map(critter => {
